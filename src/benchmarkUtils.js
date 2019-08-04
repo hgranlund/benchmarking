88c8e8302ceb255c6d0f43946dd3aecb8c3f8d5b
Simen Granlund <simen@hgranlund.com>
2019-08-04 08:53:10 +0200

    Added stringify array of array

---
+++ src/benchmarkUtils.js
@@ -0,0 +1,83 @@
+const { readFileSync, writeFileSync } = require('fs');
+const fs = require('fs');
+
+const createFolders = testRunFolder => {
+  if (!fs.existsSync(testRunFolder)) {
+    fs.mkdirSync(testRunFolder);
+  }
+};
+
+const compareWithPrev = (rawResults, name = 'test') => {
+  const results = getResults(rawResults);
+  console.log('# Current results:');
+  console.table(results);
+  var testRunFolder = `./runs/${name}/`;
+
+  createFolders(testRunFolder);
+  writeFileSync(
+    `${testRunFolder}/benchmark-run-${new Date().toISOString()}.json`,
+    JSON.stringify(results),
+  );
+
+  const path = `${testRunFolder}/benchmark-run.json`;
+  if (fs.existsSync(path)) {
+    // Do something
+    const prevResults = JSON.parse(readFileSync(path));
+    if (prevResults) {
+      console.log('# Previous run:');
+      console.table(prevResults);
+      const diff = Object.entries(results).reduce((diff, [key, result]) => {
+        diff[key] = compareWith(result, prevResults[key]);
+        return diff;
+      }, {});
+      console.log('# Diff (current-previous): ');
+      console.table(diff);
+    }
+  }
+};
+
+const fixedTo = (value, descimals) => parseFloat(value.toFixed(descimals));
+
+const compareWith = (newResult = {}, oldResult = {}) => {
+  return Object.entries(newResult).reduce((result, [key, value]) => {
+    if (['name', 'node'].includes(key)) {
+      result[key] = value;
+    } else if (key in oldResult) {
+      result[key] = fixedTo(value - oldResult[key], 2);
+    } else {
+      result[key] = NaN;
+    }
+    return result;
+  }, {});
+};
+
+const getWantedValued = obj => {
+  return {
+    '2.5%': obj.p2_5,
+    '50%': obj.p50,
+    '97.5%': obj.p97_5,
+    Average: obj.average,
+    Max: obj.max,
+    Min: obj.min,
+    StdDev: obj.stddev,
+  };
+};
+
+const getResults = event => {
+  return event.currentTarget.reduce((result, target) => {
+    const { hz, stats, name } = target;
+    const count = stats.sample.length;
+    const { mean, deviation, rme } = stats;
+    result[name] = {
+      count: Number(count),
+      'mean(ms)': fixedTo(mean * 1000, 2),
+      'deviation(ms)': fixedTo(deviation * 1000, 2),
+      'ops/sec': fixedTo(hz, 2),
+      rme: fixedTo(rme, 2),
+      node: process.versions.node,
+    };
+    return result;
+  }, {});
+};
+
+module.exports = { compareWithPrev, getResults };
---
+++ src/stringifyArrayOfArray.js
@@ -0,0 +1,40 @@
+const Benchmark = require('benchmark');
+const { compareWithPrev } = require('./benchmarkUtils');
+const stringify = require('fast-stringify');
+
+const values = new Array(10000).fill().map(() => [new Date(), null, 64]);
+
+const suite = new Benchmark.Suite();
+
+suite
+  .add('fast-stringify', () => {
+    return stringify(values).slice(1, -1);
+  })
+  .add('JSON.stringify', () => {
+    return JSON.stringify(values).slice(1, -1);
+  })
+  .add('MapAndJoin', () => {
+    return values
+      .map(value => '[' + value.map(String).join(',') + ']')
+      .join(',');
+  })
+  .add('ReduceConcat', () => {
+    return values.reduce((str, value) => {
+      str += '[' + value.map(String).join(',') + ']';
+      return str;
+    }, '');
+  })
+  .add('ForEachPush', () => {
+    let string = '';
+    values.forEach(value => {
+      string += '[' + value.map(String).join(',') + '],';
+    });
+    return string;
+  })
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  .on('complete', event => {
+    compareWithPrev(event, 'stringifyArrayOfArray');
+  })
+  .run();
